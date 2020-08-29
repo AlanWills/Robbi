1e3fd9a358db50c0055edd59179ce66ecf82f5e6
Alan Wills <alawills@googlemail.com>
2020-08-29 22:48:52 +0100

    Added loading screen wait

---
+++ Assets/Scripts/Editor/FSM/Nodes/WaitNodeEditor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Robbi.FSM.Nodes
+{
+    [CustomNodeEditor(typeof(WaitNode))]
+    public class WaitNodeEditor : FSMNodeEditor
+    {
+        #region Properties and Fields
+
+        public override Color NodeColour
+        {
+            get { return new Color(0, 0.4f, 0); }
+        }
+
+        #endregion
+    }
+}
---
+++ Assets/Scripts/Game/FSM/Nodes/WaitNode.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Robbi.FSM.Nodes
+{
+    [Serializable]
+    [CreateNodeMenu("Robbi/Wait Node")]
+    public class WaitNode : FSMNode
+    {
+        #region Properties and Fields
+
+        public float time = 1;
+
+        private float currentTime = 0;
+
+        #endregion
+
+        #region FSM Runtime Methods
+
+        protected override void OnEnter()
+        {
+            base.OnEnter();
+
+            currentTime = 0;
+        }
+
+        protected override FSMNode OnUpdate()
+        {
+            currentTime += Time.deltaTime;
+
+            return currentTime >= time ? base.OnUpdate() : null;
+        }
+
+        #endregion
+    }
+}
