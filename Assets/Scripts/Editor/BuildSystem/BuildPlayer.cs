9d61a5e547dfdaaf897c7ab94775ebd0c06e8c0d
Alan Wills <alawills@googlemail.com>
2020-09-21 16:02:42 +0100

    Tweaking build scripts

--- Assets/Scripts/Editor/BuildSystem/BuildPlayer.cs
+++ Assets/Scripts/Editor/BuildSystem/BuildPlayer.cs
@@ -10,9 +10,16 @@ namespace RobbiEditor.BuildSystem
 {
     public static class BuildPlayer
     {
-        public static void BuildAndroid()
+        public static void BuildAndroidDebug()
         {
             BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+            buildPlayerOptions.options = BuildOptions.Development | BuildOptions.AllowDebugging;
+
+            BuildAndroid(buildPlayerOptions);
+        }
+
+        private static void BuildAndroid(BuildPlayerOptions buildPlayerOptions)
+        {
             buildPlayerOptions.locationPathName = "Builds/Robbi.apk";
             buildPlayerOptions.scenes = EditorBuildSettings.scenes.Select(x => x.path).ToArray();
             buildPlayerOptions.target = BuildTarget.Android;
