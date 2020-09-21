e8d3b06eaa5f72636bec1db5970961ac591f8f4c
Alan Wills <alawills@googlemail.com>
2020-09-21 15:53:20 +0100

    Trying to add support for jenkins automated builds

---
+++ Assets/Scripts/Editor/BuildSystem/BuildPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+namespace RobbiEditor.BuildSystem
+{
+    public static class BuildPlayer
+    {
+        public static void BuildAndroid()
+        {
+            BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+            buildPlayerOptions.locationPathName = Environment.GetCommandLineArgs().Last();
+            buildPlayerOptions.scenes = EditorBuildSettings.scenes.Select(x => x.path).ToArray();
+            buildPlayerOptions.target = BuildTarget.Android;
+            buildPlayerOptions.targetGroup = BuildTargetGroup.Android;
+
+            BuildPipeline.BuildPlayer(buildPlayerOptions);
+        }
+    }
+}
