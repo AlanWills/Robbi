3c6ebae554668ee34a2b428ea346dd3ba9171f4e
Alan Wills <alawills@googlemail.com>
2020-09-27 21:57:30 +0100

    Improved dialog flow

--- Assets/Scripts/Editor/FSM/FSMGraphEditor.cs
+++ Assets/Scripts/Editor/FSM/FSMGraphEditor.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEditor;
 using UnityEngine;
+using XNode;
 using XNodeEditor;
 
 namespace Robbi.FSM
@@ -18,6 +19,20 @@ namespace Robbi.FSM
         public override string GetNodeMenuName(Type type)
         {
             return typeof(FSMNode).IsAssignableFrom(type) ? base.GetNodeMenuName(type) : null;
+        }
+
+        #endregion
+
+        #region Add/Remove/Copy
+
+        public override void RemoveNode(Node node)
+        {
+            base.RemoveNode(node);
+
+            if (target.nodes.Count == 1)
+            {
+                (target as FSMGraph).startNode = target.nodes[0] as FSMNode;
+            }
         }
 
         #endregion
