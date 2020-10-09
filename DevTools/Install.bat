e8c0fa0d146dbd7204e03616d18f226dc46bbab9
Alan Wills <alawills@googlemail.com>
2020-10-10 00:35:58 +0100

    Fixing up install script

--- DevTools/Install.bat
+++ DevTools/Install.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Unity\Hub\Editor\2020.1.1f1\Editor\Data\PlaybackEngines\AndroidPlayer\SDK\platform-tools\adb.exe" install "%1\Builds\Robbi.apk"
+"C:\Program Files\Unity\Hub\Editor\2020.1.1f1\Editor\Data\PlaybackEngines\AndroidPlayer\SDK\platform-tools\adb.exe" install "%CD%\Builds\Robbi.apk"
