afefe8ac22c88debc3b96e48df4ed26fe2b60f76
Alan Wills <alawills@googlemail.com>
2020-10-19 10:56:19 +0100

    Adding more coloured door assets

---
+++ Assets/Scripts/Game/Events/Level/TileEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Tilemaps;
+
+namespace Robbi.Events
+{
+    [Serializable]
+    public class TileUnityEvent : UnityEvent<GameObject> { }
+
+    [Serializable]
+    [CreateAssetMenu(fileName = "TileEvent", menuName = "Robbi/Events/Tile Event")]
+    public class TileEvent : ParameterisedEvent<Tile>
+    {
+    }
+}
