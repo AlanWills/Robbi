ff292bee03836634e67fb7e8568d7e8d70044519
Alan Wills <alawills@googlemail.com>
2020-11-10 22:01:59 +0000

    Fixed switches colour

---
+++ Assets/Scripts/Editor/Constants/DoorColours.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace RobbiEditor.Constants
+{
+    public static class DoorColours
+    {
+        public static readonly Color GREEN = new Color(41.0f / 255, 1.0f, 0);
+        public static readonly Color RED = new Color(1, 0, 41.0f / 255);
+        public static readonly Color BLUE = new Color(0, 140.0f / 255, 1);
+        public static readonly Color GREY = new Color(90.0f / 255, 90.0f / 255, 90.0f / 255);
+    }
+}
