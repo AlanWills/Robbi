76dd29db006771ad69125ff2b5726a5e1d1fd9c2
Alan Wills <alawills@googlemail.com>
2020-11-22 23:41:43 +0000

    Working on bumping version numbers when building

--- Assets/Scripts/Editor/BuildSystem/BuildPlayer.cs
+++ Assets/Scripts/Editor/BuildSystem/BuildPlayer.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEditor;
 using UnityEngine.SceneManagement;
+using static RobbiEditor.BuildSystem.BuildVersion;
 
 namespace RobbiEditor.BuildSystem
 {
@@ -14,11 +15,12 @@ namespace RobbiEditor.BuildSystem
         public static void BuildAndroidDebug()
         {
             EditorUserBuildSettings.development = true;
-            
+
             BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
             buildPlayerOptions.options = BuildOptions.Development | BuildOptions.AllowDebugging | BuildOptions.StrictMode;
             
-            Build(buildPlayerOptions, BuildTargetGroup.Android, BuildTarget.Android, "Builds/Android/Robbi.apk");
+            Build(buildPlayerOptions, BuildTargetGroup.Android, BuildTarget.Android, "Builds/Android/Robbi.apk", ParseVersion(PlayerSettings.Android.bundleVersionCode));
+            BumpAndroidVersion();
         }
 
         [MenuItem("Robbi/Builds/Windows Debug")]
@@ -30,7 +32,8 @@ namespace RobbiEditor.BuildSystem
             BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
             buildPlayerOptions.options = BuildOptions.Development | BuildOptions.AllowDebugging;
 
-            Build(buildPlayerOptions, BuildTargetGroup.Standalone, BuildTarget.StandaloneWindows64, "Builds/Windows/Robbi.exe");
+            Build(buildPlayerOptions, BuildTargetGroup.Standalone, BuildTarget.StandaloneWindows64, "Builds/Windows/Robbi.exe", ParseVersion(PlayerSettings.macOS.buildNumber));
+            BumpWindowsVersion();
         }
 
         [MenuItem("Robbi/Builds/iOS Debug")]
@@ -42,14 +45,16 @@ namespace RobbiEditor.BuildSystem
             BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
             buildPlayerOptions.options = BuildOptions.Development | BuildOptions.AllowDebugging;
 
-            Build(buildPlayerOptions, BuildTargetGroup.iOS, BuildTarget.iOS, "Builds/iOS");
+            Build(buildPlayerOptions, BuildTargetGroup.iOS, BuildTarget.iOS, "Builds/iOS", ParseVersion(PlayerSettings.iOS.buildNumber));
+            BumpiOSVersion();
         }
 
         private static void Build(
             BuildPlayerOptions buildPlayerOptions,
             BuildTargetGroup buildTargetGroup, 
             BuildTarget buildTarget,
-            string locationPathName)
+            string locationPathName,
+            Version newVersion)
         {
             EditorUserBuildSettings.SwitchActiveBuildTarget(buildTargetGroup, buildTarget);
 
@@ -58,6 +63,7 @@ namespace RobbiEditor.BuildSystem
             buildPlayerOptions.target = buildTarget;
             buildPlayerOptions.targetGroup = buildTargetGroup;
 
+            PlayerSettings.bundleVersion = newVersion.ToString();
             BuildPipeline.BuildPlayer(buildPlayerOptions);
         }
     }
---
+++ Assets/Scripts/Editor/BuildSystem/BuildVersion.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+namespace RobbiEditor.BuildSystem
+{
+    public static class BuildVersion
+    {
+        [MenuItem("Robbi/Builds/Version/Bump Android")]
+        public static void BumpAndroidVersion()
+        {
+            Version version = ParseVersion(PlayerSettings.Android.bundleVersionCode);
+            PlayerSettings.Android.bundleVersionCode = version.Major * 10000 + version.Minor * 100 + version.Build + 1;
+            PlayerSettings.bundleVersion = version.ToString();
+        }
+
+        [MenuItem("Robbi/Builds/Version/Bump Windows")]
+        public static void BumpWindowsVersion()
+        {
+            // Bump windows version number - yes we're using the mac version number, but windows doesn't have a unique one apparently
+            Version version = ParseVersion(PlayerSettings.macOS.buildNumber);
+            version = new Version(version.Major, version.Minor, version.Build + 1);
+            PlayerSettings.macOS.buildNumber = version.ToString();
+            PlayerSettings.bundleVersion = version.ToString();
+        }
+
+        [MenuItem("Robbi/Builds/Version/Bump iOS")]
+        public static void BumpiOSVersion()
+        {
+            Version version = ParseVersion(PlayerSettings.iOS.buildNumber);
+            version = new Version(version.Major, version.Minor, version.Build + 1);
+            PlayerSettings.iOS.buildNumber = version.ToString();
+            PlayerSettings.bundleVersion = version.ToString();
+        }
+
+        public static Version ParseVersion(int bundleVersionCode)
+        {
+            int major = bundleVersionCode / 10000;
+            int minor = (bundleVersionCode - major * 10000) / 100;
+            int patch = bundleVersionCode - major * 10000 - minor * 100;
+
+            return new Version(major, minor, patch);
+        }
+
+        public static Version ParseVersion(string bundleString)
+        {
+            return new Version(bundleString);
+        }
+    }
+}
