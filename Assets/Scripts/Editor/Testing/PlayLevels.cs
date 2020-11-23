66b116440b516e50892a6861fff70b992fc72b18
Alan Wills <alawills@googlemail.com>
2020-11-23 19:13:48 +0000

    Tweaks to support IT

--- Assets/Scripts/Editor/Testing/PlayLevels.cs
+++ Assets/Scripts/Editor/Testing/PlayLevels.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace RobbiEditor.Testing
 {
@@ -16,6 +17,7 @@ namespace RobbiEditor.Testing
         [MenuItem("Robbi/Testing/Play Levels")]
         public static void MenuItem()
         {
+            EditorSceneManager.OpenScene("Assets/Scenes/Splash.unity", OpenSceneMode.Single);
             IntegrationTestRunner.Instance.RunTest("PlayLevels");
         }
     }
