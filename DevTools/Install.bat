208739398a317052689681227884c7176fc435d8
Alan Wills <alawills@googlemail.com>
2020-11-23 10:47:10 +0000

    Updating android install script

--- DevTools/Install.bat
+++ DevTools/Install.bat
@@ -1,1 +1,3 @@
-"C:\Program Files\Unity\Hub\Editor\2020.1.1f1\Editor\Data\PlaybackEngines\AndroidPlayer\SDK\platform-tools\adb.exe" install "%CD%\Builds\Android\Robbi.apk"
+set version=%1
+
+"C:\Program Files\Unity\Hub\Editor\2020.1.1f1\Editor\Data\PlaybackEngines\AndroidPlayer\SDK\platform-tools\adb.exe" install "%CD%\Builds\Android\Robbi-%version%.apk"
