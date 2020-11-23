1f345ee38962e5d260e5631024a1dba021e9fbd1
Alan Wills <alawills@googlemail.com>
2020-11-23 11:05:15 +0000

    Updated install script to accept a version or use latest build version

--- DevTools/Install.bat
+++ DevTools/Install.bat
@@ -1,3 +1,9 @@
-set version=%1
+if "%~1"=="" (
+	set PATH="%CD%\Builds\Android\Robbi-%1%.apk"
+) else (
+    set /p PATH=<"%CD%\Builds\Android\BUILD_LOCATION.txt"
+)
 
-"C:\Program Files\Unity\Hub\Editor\2020.1.1f1\Editor\Data\PlaybackEngines\AndroidPlayer\SDK\platform-tools\adb.exe" install "%CD%\Builds\Android\Robbi-%version%.apk"
+echo %PATH%
+
+"C:\Program Files\Unity\Hub\Editor\2020.1.1f1\Editor\Data\PlaybackEngines\AndroidPlayer\SDK\platform-tools\adb.exe" install "%PATH%"
