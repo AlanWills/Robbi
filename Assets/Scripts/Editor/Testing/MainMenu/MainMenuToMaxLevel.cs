cabd72b421090c916d1425c775fcc09993dd7b6e
Alan Wills <alawills@googlemail.com>
2020-11-26 23:21:16 +0000

    Added final integration tests

---
+++ Assets/Scripts/Editor/Testing/MainMenu/MainMenuToMaxLevel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+
+namespace RobbiEditor.Testing
+{
+    public class MainMenuToMaxLevel
+    {
+        [MenuItem("Robbi/Testing/Main Menu/Main Menu To Max Level")]
+        public static void MenuItem()
+        {
+            IntegrationTestEditorAPI.RunTest<MainMenuToMaxLevel>();
+        }
+    }
+}
