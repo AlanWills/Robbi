ca88b9306b71ad4fe37a5bad1dd95234f147d274
Alan Wills <alawills@googlemail.com>
2020-11-30 10:36:17 +0000

    Added time taken to end level dialog

---
+++ Assets/Scripts/Game/Timing/TimeManager.cs
@@ -0,0 +1,35 @@
+﻿using Robbi.Managers;
+using Robbi.Parameters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Robbi.Timing
+{
+    [AddComponentMenu("Robbi/Time/Time Manager")]
+    public class TimeManager : NamedManager
+    {
+        #region Properties and Fields
+
+        public FloatValue timeTaken;
+
+        #endregion
+
+        #region Unity Methods
+
+        private void Start()
+        {
+            timeTaken.value = 0;
+        }
+
+        private void Update()
+        {
+            timeTaken.value += Time.deltaTime;
+        }
+
+        #endregion
+    }
+}
