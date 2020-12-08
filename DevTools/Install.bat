66cc017e9a01a0019f042d854b29a0a088596c1c
Alan Wills <alawills@googlemail.com>
2020-12-08 14:16:34 +0000

    Deleted install script

--- DevTools/Install.bat
+++
@@ -1,9 +0,0 @@
-if NOT "%~1"=="" (
-	set PATH="%CD%\Builds\Android\Robbi-%1%.apk"
-) else (
-    set /p PATH=<"%CD%\Builds\Android\BUILD_LOCATION.txt"
-)
-
-echo %PATH%
-
-"C:\Program Files\Unity\Hub\Editor\2020.1.1f1\Editor\Data\PlaybackEngines\AndroidPlayer\SDK\platform-tools\adb.exe" install "%PATH%"
