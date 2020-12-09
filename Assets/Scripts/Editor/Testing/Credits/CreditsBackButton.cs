4ca173711850c1aaed868532063881b190e6f040
Alan Wills <alawills@googlemail.com>
2020-12-09 10:24:44 +0000

    Added more integration tests

---
+++ Assets/Scripts/Editor/Testing/Credits/CreditsBackButton.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+
+namespace RobbiEditor.Testing
+{
+    public class CreditsBackButton : IIntegrationTest
+    {
+        [MenuItem("Robbi/Testing/Credits/Credits Back Button")]
+        public static void MenuItem()
+        {
+            IntegrationTestEditorAPI.RunTest<CreditsBackButton>();
+        }
+    }
+}
---
+++ Assets/Scripts/Editor/Testing/MainMenu/MainMenuToCredits.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+
+namespace RobbiEditor.Testing
+{
+    public class MainMenuToCredits : IIntegrationTest
+    {
+        [MenuItem("Robbi/Testing/Main Menu/Main Menu To Credits")]
+        public static void MenuItem()
+        {
+            IntegrationTestEditorAPI.RunTest<MainMenuToCredits>();
+        }
+    }
+}
