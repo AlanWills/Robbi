3231a3388a6b77e22de28bc0da5dafe7673b25dc
Alan Wills <alawills@googlemail.com>
2020-12-10 21:38:41 +0000

    Working on level station selection

---
+++ Assets/Scripts/Game/PickLevel/LevelStationManager.cs
@@ -0,0 +1,41 @@
+﻿using Robbi.Parameters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Robbi.PickLevel
+{
+    [AddComponentMenu("Robbi/Pick Level/Level Station Manager")]
+    public class LevelStationManager : MonoBehaviour
+    {
+        #region Properties and Fields
+
+        [Header("UI")]
+        [SerializeField]
+        private GameObject levelStationPrefab;
+
+        [Header("Parameters")]
+        [SerializeField]
+        private UIntValue currentLevel;
+
+        [SerializeField]
+        private UIntValue latestLevel;
+
+        #endregion
+
+        #region Unity Methods
+
+        private void Awake()
+        {
+            for (int i = 0; i < latestLevel.Value * 3; ++i)
+            {
+                GameObject.Instantiate(levelStationPrefab, transform);
+            }
+        }
+
+        #endregion
+    }
+}
