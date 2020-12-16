89d9295def0a9b6e8272d5748266bd584bdd3810
Alan Wills <alawills@googlemail.com>
2020-12-16 19:22:58 +0000

    Added two new validation conditions

---
+++ Assets/Scripts/Editor/Validation/InteractableStateMachines/Conditions/InteractableStateMachineStatesNotNull.cs
@@ -0,0 +1,50 @@
+﻿using Robbi.Levels.Elements;
+using Robbi.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobbiEditor.Validation.Interactables.Conditions
+{
+    public class InteractableStateMachineStatesNotNull : IValidationCondition<InteractableStateMachine>, IFixableCondition<InteractableStateMachine>
+    {
+        public string DisplayName { get { return "States Not Null"; } }
+
+        public bool CanFix(InteractableStateMachine asset)
+        {
+            return true;
+        }
+
+        public void Fix(InteractableStateMachine stateMachine, StringBuilder output)
+        {
+            for (int i = stateMachine.NumStates - 1; i >= 0 ; --i)
+            {
+                Interactable state = stateMachine.GetState(i);
+                if (state == null)
+                {
+                    stateMachine.RemoveState(i);
+                    output.AppendLineFormat("Interactable State Machine {0} has a state removed at index {1}", stateMachine.name, i.ToString());
+                }
+            }
+        }
+
+        public bool Validate(InteractableStateMachine stateMachine, StringBuilder output)
+        {
+            bool valid = true;
+
+            for (int i = 0; i < stateMachine.NumStates; ++i)
+            {
+                Interactable state = stateMachine.GetState(i);
+                if (state == null)
+                {
+                    valid = false;
+                    output.AppendLineFormat("Interactable State Machine {0} has a state at index {1} which is invalid", stateMachine.name, i.ToString());
+                }
+            }
+
+            return valid;
+        }
+    }
+}
---
+++ Assets/Scripts/Editor/Validation/Interactables/Conditions/InteractableModifiersNotNull.cs
@@ -0,0 +1,48 @@
+﻿using Robbi.Levels.Elements;
+using Robbi.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobbiEditor.Validation.Interactables.Conditions
+{
+    public class InteractableModifiersNotNull : IValidationCondition<Interactable>, IFixableCondition<Interactable>
+    {
+        public string DisplayName { get { return "Modifiers Not Null"; } }
+
+        public bool CanFix(Interactable asset)
+        {
+            return true;
+        }
+
+        public void Fix(Interactable interactable, StringBuilder output)
+        {
+            for (int i = interactable.NumInteractedModifiers - 1; i >= 0; --i)
+            {
+                if (interactable.GetInteractedModifier(i) == null)
+                {
+                    interactable.RemoveInteractedModifier(i);
+                    output.AppendLineFormat("Interactable {0} has a null modifier removed at index {1}", interactable.name, i.ToString());
+                }
+            }
+        }
+
+        public bool Validate(Interactable interactable, StringBuilder output)
+        {
+            bool valid = true;
+
+            for (int i = 0; i < interactable.NumInteractedModifiers; ++i)
+            {
+                if (interactable.GetInteractedModifier(i) == null)
+                {
+                    valid = false;
+                    output.AppendLineFormat("Interactable {0} has a null modifier at index {1}", interactable.name, i.ToString());
+                }
+            }
+
+            return valid;
+        }
+    }
+}
