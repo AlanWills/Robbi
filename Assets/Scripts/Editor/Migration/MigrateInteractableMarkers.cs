809a844fb92308b66ac223e4049c7b55ff63aa82
Alan Wills <alawills@googlemail.com>
2021-01-01 17:22:04 +0000

    Working on new interactable markers

---
+++ Assets/Scripts/Editor/Migration/MigrateInteractableMarkers.cs
@@ -0,0 +1,43 @@
+﻿using Celeste.Tilemaps;
+using Robbi.Levels;
+using RobbiEditor.Iterators;
+using RobbiEditor.Levels;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace RobbiEditor.Migration
+{
+    public static class MigrateInteractableMarkers
+    {
+        [MenuItem("Robbi/Migration/Migrate Interactable Markers")]
+        public static void MenuItem()
+        {
+            foreach (LevelFolder levelFolder in new LevelFolders())
+            {
+                if (levelFolder.Index != 9)
+                {
+                    continue;
+                }
+
+                GameObject levelPrefab = AssetDatabase.LoadAssetAtPath<GameObject>(levelFolder.PrefabPath);
+                Transform interactables = levelPrefab.transform.Find("Interactables");
+
+                for (int i = interactables.childCount - 1; i >= 0; i -= 2)
+                {
+                    Transform newMarker = interactables.GetChild(i);
+                    Transform oldMarker = interactables.GetChild(i - 1);
+                    newMarker.localPosition = oldMarker.localPosition;
+
+                    GameObject.DestroyImmediate(oldMarker.gameObject, true);
+                    EditorUtility.SetDirty(newMarker);
+                }
+
+                EditorUtility.SetDirty(levelPrefab);
+            }
+
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+        }
+    }
+}
