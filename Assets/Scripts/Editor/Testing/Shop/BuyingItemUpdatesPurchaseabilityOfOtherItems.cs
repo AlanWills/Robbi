3abb4ea8f8d30c5aadbf9db4623d9c1ab227de63
Alan Wills <alawills@googlemail.com>
2021-01-12 13:25:29 +0000

    Added one last integration test for shop

---
+++ Assets/Scripts/Editor/Testing/Shop/BuyingItemUpdatesPurchaseabilityOfOtherItems.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+
+namespace RobbiEditor.Testing
+{
+    public class BuyingItemUpdatesPurchaseabilityOfOtherItems : IIntegrationTest
+    {
+        [MenuItem("Robbi/Testing/Shop/Buying Item Updates Purchaseability Of Other Items")]
+        public static void MenuItem()
+        {
+            IntegrationTestEditorAPI.RunTest<BuyingItemUpdatesPurchaseabilityOfOtherItems>();
+        }
+    }
+}
