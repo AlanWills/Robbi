0e3f21a4d67e4b3fdd3fe5e9bb9c44e395d7f2a9
Alan Wills <alawills@googlemail.com>
2021-01-13 22:44:57 +0000

    Rolled out UI sfx

---
+++ Assets/Celeste/Events/Audio/AudioClipEventRaiser.cs
@@ -0,0 +1,15 @@
+﻿using Celeste.Parameters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Celeste.Events
+{
+    [AddComponentMenu("Celeste/Events/Audio/Audio Clip Event Raiser")]
+    public class AudioClipEventRaiser : ParameterisedEventRaiser<AudioClip, AudioClipEvent>
+    {
+    }
+}
