eb79fc4d464b88cdea5d2b381c4a2bee8d45cf33
Alan Wills <alawills@googlemail.com>
2021-02-06 11:20:40 +0000

    Modified tutorials so they are only displayed once

---
+++ Assets/Scripts/Editor/Migration/MigrateTutorial.cs
@@ -0,0 +1,40 @@
+﻿using Celeste.Parameters;
+using Celeste.Utils;
+using Celeste.Viewport;
+using Robbi.Levels;
+using Robbi.Tutorials;
+using RobbiEditor.Iterators;
+using RobbiEditor.Levels;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.UI;
+
+namespace RobbiEditor.Migration
+{
+    public static class MigrateTutorials
+    {
+        [MenuItem("Robbi/Migration/Migrate Tutorials")]
+        public static void MenuItem()
+        {
+            foreach (LevelFolder levelFolder in new LevelFolders())
+            {
+                if (!Directory.Exists(levelFolder.TutorialsFolderPath))
+                {
+                    continue;
+                }
+
+                GameObject tutorialsPrefab = AssetDatabase.LoadAssetAtPath<GameObject>(levelFolder.TutorialsPrefabPath);
+                Tutorial tutorial = tutorialsPrefab.AddComponent<Tutorial>();
+                tutorial.latestUnlockedLevel = AssetDatabase.LoadAssetAtPath<UIntValue>(ParameterFiles.LATEST_UNLOCKED_LEVEL);
+                tutorial.currentLevel = AssetDatabase.LoadAssetAtPath<UIntValue>(ParameterFiles.CURRENT_LEVEL);
+
+                EditorUtility.SetDirty(tutorialsPrefab);
+            }
+
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+        }
+    }
+}
