88b246e48b400df9b2e7208035dde847cbdd91ea
Alan Wills <alawills@googlemail.com>
2024-03-24 22:29:59 +0000

    Working on an addressable build

--- Assets/Scripts/Editor/RobbiEditorConstants.cs
+++ Assets/Scripts/Editor/RobbiEditorConstants.cs
@@ -17,7 +17,7 @@ namespace RobbiEditor.Bootstrap
         public const string OPTIONS_SCENE_SET_PATH = "Assets/Options/Scenes/OptionsSceneSet.asset";
         public const string OPTIONS_SCENE_MENU_ITEM = "Robbi/Scenes/Load Options";
 
-        public const string CREDITS_SCENE_SET_PATH = "Assets/Credits/Scenes/CreditsSceneSet.asset";
+        public const string CREDITS_SCENE_SET_PATH = "Assets/MainMenu/Scenes/CreditsSceneSet.asset";
         public const string CREDITS_SCENE_MENU_ITEM = "Robbi/Scenes/Load Credits";
 
         public const string SHOP_SCENE_SET_PATH = "Assets/Shop/Scenes/ShopSceneSet.asset";
